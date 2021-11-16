1611b2adeee6f3a69e24fc87c06fc74c8913f891
Zain Aldeen Fayod <zainaldeenfayod@gmail.com>
2021-11-16 22:59:32 +0200

    add layouts

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const adminData = require('./admin');
 
 router.get('/', (req, res,next) => {
     const products = adminData.products;
-    res.render('shop', {pageTitle: 'Shop', prods: products});
+    res.render('shop', {pageTitle: 'Shop', prods: products, path: '/'});
 })
 
 module.exports = router;
